5ff8e54a5e6b5f616240df43a2d70b8b10f757fe
Jesús Giganto Diez <Jesus.Giganto@tajamar365.com>
2018-02-21 23:43:13 +0100

    A

---
+++ MVCAlmacenarEmpleadosSesion/Controllers/EmpleadosController.cs
@@ -0,0 +1,58 @@
+﻿using MVCAlmacenarEmpleadosSesion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAlmacenarEmpleadosSesion.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        ModeloHospital modelo;
+        public EmpleadosController()
+        {
+            modelo = new ModeloHospital();
+        }
+        
+        public ActionResult AlmacenarEmpleados(int? empno)
+        {
+            IQueryable<EMP> empleados = modelo.GetEmpleados();
+            if (empno != null)
+            {
+                List<int> listaempleados;
+                if (Session["EMPLEADOS"] == null)
+                {
+                    listaempleados = new List<int>();
+                }
+                else
+                {
+                    listaempleados = (List<int>)Session["EMPLEADOS"];
+                }
+                listaempleados.Add(empno.GetValueOrDefault());
+                Session["EMPLEADOS"] = listaempleados;
+                ViewBag.Mensaje = "Empleados almacenados: " + listaempleados.Count();
+            }
+            return View(empleados);
+        }
+        public ActionResult MostrarEmpleados()
+        {
+            if (Session["EMPLEADOS"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                List<int> lista = (List<int>)Session["EMPLEADOS"];
+                IQueryable empleados =
+                    this.modelo.GetEmpleadosSession(lista);
+                return View(empleados);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCAlmacenarEmpleadosSesion/Models/ModeloHospital.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAlmacenarEmpleadosSesion.Models
+{
+    public class ModeloHospital
+    {
+        ContextoHospital contexto;
+        public ModeloHospital()
+        {
+            contexto = new ContextoHospital();
+        }
+
+        public IQueryable<EMP> GetEmpleados()
+        {
+            var consulta = from datos in this.contexto.EMP
+                           select datos;
+            return consulta;
+
+        }
+        public IQueryable<EMP> GetEmpleadosSession(List<int> empleados)
+        {
+            var consulta = from datos in this.contexto.EMP
+                           where empleados.Contains(datos.EMP_NO)
+                           select datos;
+            return consulta;
+        }
+    }
+}
